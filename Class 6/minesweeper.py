c64ff40bfe84a41235cca1499fb1a9fab9e4e44a
kennethgoodman <kennethsgoodman@yahoo.com>
2015-12-29 23:28:46 -0500

    minesweeper done

---
+++ Class 6/minesweeper.py
@@ -0,0 +1,26 @@
+from minesweeperHelper import * # gives access to helper functions:
+
+#Helper Functions:
+# createBoard(rows, columns)
+# putBomb(board,row,column)
+# loopThroughBoardAndDo(board, function_on_each_element, function_after_each_row) - takes a board and two functions, 
+# 						the first functions gets called on each element with three arguements, the board, row and columns, the second, gets 
+#						called on each row, and has two arguements, the board and row
+#						ex: loopThroughBoardAndDo(board, putBomb , None) will place put a bomb at each place
+# putBombsInBoard(board, bombs)
+# printBoard(board)
+
+def fillInBoard(board):
+	# The board should be filled so that each element that is not a * has the number of * directly adjacent in the four cardinal directions
+	# and the four diagonals
+	# Implement this function to have the board print out the correct number in each box
+	# IE: 	* - -
+	#	 	- * -
+	#		- - -
+	#		- - *
+	# Should fill the board so that the board looks like:
+	#		* 2 1
+	#		2 * 1
+	#		1 2 1
+	#		0 1 *
+	return
---
+++ Class 6/minesweeperHelper.py
@@ -0,0 +1,34 @@
+import random #for random
+def createBoard(rows, columns):
+	if rows <= 0: # if user puts bad input
+		raise NameError("need to have more than zero rows")
+	return [["-"]*columns for i in range(rows)]
+def putBomb(board,row,column):
+	board[row][column] = "*"
+def loopThroughBoardAndDo(board, function_on_each_element, function_after_each_row):
+	rows = len(board[0])
+	for row in range(len(board)):
+		for column in range(rows):
+			if(function_on_each_element):
+				function_on_each_element(board,row ,column)
+		if(function_after_each_row):
+			function_after_each_row(board,row)
+def putBombsInBoard(board, bombs):
+ 	if bombs >= len(board)*len(board[0]): # if the user wants as many bombs as their are spaces or more, fill the board with bombs
+ 		loopThroughBoardAndDo(board, putBomb , None) 
+ 	else:
+ 		bombsLeftToPlace = float(bombs) #convert to float so that we can do fractional math with it
+ 		columns = len(board[0])
+ 		rows = len(board)
+ 		row = column = 0
+ 		while bombsLeftToPlace > 0:
+ 			if(random.random() > (bombsLeftToPlace/bombs - .1) and board[row][column] != "*"):
+ 				board[i][j] = "*"
+ 				bombsLeftToPlace = bombsLeftToPlace - 1.0
+ 			column = (column + 1)%columns #increment the column
+ 			if column == 0: #if on the first column
+ 				row = (row+1)%rows
+def printBoard(board):
+	loopThroughBoardAndDo(board,
+		lambda board,row,column: print(board[row][column],end=""), # function to print each element without a new line
+		lambda a,b: print("")) 								   # function to print a new line at the beggining of each row
