b3c678a156656f95974e2f197587bc84723079d2
Kenneth Goodman <kennethsgoodman@yahoo.com>
2016-01-08 18:58:25 +0000

    Added comments, need to implement play function

--- Class 5/towerOfHanoi.py
+++ Class 5/towerOfHanoi.py
@@ -1,3 +1,4 @@
+#prints the tower for towers of Hanoi. 
 def printTowers(towers):
 	maxLength = max(len(towers[0]),len(towers[1]),len(towers[2]))
 	for i in range(maxLength-1,-1,-1):
@@ -5,23 +6,34 @@ def printTowers(towers):
 			if(i == 0 and len(towers[j]) == 0):
 				print("_",end="  ")
 			else:
-				try:
+				try: 
 					print(str(towers[j][i]), end="  ")
 				except:
 					print("   ", end="")
-		print("")
+		print("") #print new line
+#create three lists, the first with n rings and the other two, empty
 def createTowers(number_of_rings):
 	return [[i for i in range(number_of_rings,0,-1)],[],[]]
+# move the top ring:
+# if the tower isn't empty and, 
+#         the tower we are moving to is empty or it's top 
+#         ring is larger than the tower we are moving from:
+#              move the ring
+# else the legality check failed and we throw an error
 def move(towers, fromTower, toTower):
 	if(len(towers[fromTower]) != 0 and (len(towers[toTower]) == 0 or towers[fromTower][-1] < towers[toTower][-1])):
 		towers[toTower].append(towers[fromTower].pop())
 	else:
 		raise NameError("Illegal move")
+# solve the problem recursively, move the top n-1 rings from
+# the starting tower to the temp tower,
+# then move n-1 rings from that tower to correct tower
 def solve(height,towers, fromTower, toTower, withTower):
 	if height >= 1:
 		solve(height-1,towers, fromTower,withTower,toTower)
 		move(towers,fromTower,toTower)
 		solve(height-1,towers, withTower,toTower, fromTower)
+# need to implement 
 def playTowersOfHanoi():
         print("playing")
 	return
