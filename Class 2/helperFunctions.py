6ba1c9103ad9e6e00737d88ec0d16b46a259edbc
kennethgoodman <kennethsgoodman@yahoo.com>
2016-01-21 07:32:36 -0500

    adding some new files

--- Class 2/helperFunctions.py
+++ Class 2/helperFunctions.py
@@ -1,8 +1,15 @@
-def between(start, end):
+#uses ord() to convert the two characters to their corresponding ascii values, 
+# then get that range of numbers. 
+#Having the range of numbers (that represent the characters in ascii) it will map those numbers 
+# back their corresponding character values and put it in a list. 
+# the first list is lower case. The second is uppercase. 
+# this function currently does no testing against incorrect input(such as numbers, two or more letter strings, backwards ranges (d->a), etc. 
+def between(start, end): 
 	return list(map(chr, range(ord(start.lower()),ord(end.lower())+1))) + list(map(chr, range(ord(start.upper()), ord(end.upper())+1)))
-if 'c' in between('a','d'):
+# examples:
+if 'c' in between('a','d'): #show that it works with lowercase
 	print("c is in between")
-if 'C' in between('a','d'):
+if 'C' in between('a','d'): #works with uppercase
 	print("C is in between")
-if 'f' not in between('a','d'):
+if 'f' not in between('a','d'): #show that will sometimes come up false
 	print("f is not in between")
--- Class 4/classFile.py
+++ Class 4/classFile.py
@@ -1,15 +1,16 @@
 def between(start, end):
-	return list(map(int, range(start, end+1)))
+	return list(map(int, range(start, end+1))) #returns list of numbers between start and end
+# will eventually combine this other between() for characters for more English-like code 
 def aNumber(nbr):
 	return nbr.isdigit()
 def getName():
 	name = input('Enter a name: ')
-	while not all(letter.isalpha() or letter.isspace() for letter in name):
+	while not all(character.isalpha() or character.isspace() for character in name):
 		name = input(str(name) + " is not a name that only contains letters or spaces, re-enter: ")
 	return name
 def getAge():
 	age = input('Enter your age: ')
-	while not aNumber(age) or float(age) not in between(0,120): #if number between 0 and 120
+	while not aNumber(age) or float(age) not in between(0,120): #if number not between 0 and 120
 		age = input(str(age) + " is not a number or reasonable age, re-enter: ")
 	return float(age)
 def findMax(array):
@@ -17,11 +18,18 @@ def findMax(array):
 	for (index, nbr) in enumerate(array):
 		if float(nbr) == maxNbr:
 			return index
-def maxAge(numberOfStudents):
+#change function to accept any string for any property
+def getStudents(numberOfStudents):
+       #maybe change to list of tuples instead of two lists
 	names = []
 	ages = []
 	for i in range(numberOfStudents):
 		names.append(getName())
 		ages.append(getAge())
-	maxAge = findMax(ages)
-	return [names[maxAge], ages[maxAge]]
+        return names,ages #was space indented, check if works
+def maxProperty(names, property):
+	indexOfMaxProperty = findMax(property) #returns index of highest age 
+	return [names[indexOfMaxProperty], property[indexOfMaxProperty]]
+#example:
+names, ages = getStudents(3)
+print( maxAge(names, ages) ) 
