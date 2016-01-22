34ec5f5875932c274001568939d26219a609517b
kennethgoodman <kennethsgoodman@yahoo.com>
2016-01-22 13:57:30 -0500

    added examples and debug argument

--- Class 7/bubblesort.py
+++ Class 7/bubblesort.py
@@ -1,4 +1,4 @@
-def bubblesort(array):
+def bubblesort(array, debug = False):
 	#ex: 
 	# input:[3,6,1,5]
 	# 1 - [3,6,1,5] - > [3,1,6,5] - > [3,1,5,6]
@@ -7,28 +7,51 @@ def bubblesort(array):
 	while stillSwapping:
 		stillSwapping = False #assume we should stop
 		for i in range(1, len(array)):
+			if debug:
+				print("New loop: " + str(array))
 			if array[i-1] > array[i]:
 				array[i-1], array[i] = array[i], array[i-1] #swap
+				if debug:
+					print("Swapping: " + str(array))
 				stillSwapping = True #dont stop since we swapped
 	return array
-def bubblesortOptimized1(array):
+def bubblesortOptimized1(array, debug = False):
 	stillSwapping = True
 	length = len(array)
 	while stillSwapping:
 		stillSwapping = False #assume we should stop
 		for i in range(1, length):
+			if debug:
+				print("New loop: " + str(array))
 			if array[i-1] > array[i]:
 				array[i-1], array[i] = array[i], array[i-1] #swap
+				if debug:
+					print("Swapping: " + str(array))
 				stillSwapping = True #dont stop since we swapped
 		length = length - 1
 	return array
-def bubblesortOptimized2(array):
+def bubblesortOptimized2(array,debug=False):
 	length = len(array)
 	while length != 0:
 		newLength = 0 # assume we dont make any more swaps
 		for i in range(1, length):
+			if debug:
+				print("New loop: " + str(array))
 			if array[i-1] > array[i]:
 				array[i-1], array[i] = array[i], array[i-1] #swap
+				if debug:
+					print("Swapping: " + str(array))
 				newLength = i #change assumptions, to dont make any more past this
 		length = newLength #dont need to check past the last swap
 	return array
+
+# Examples
+array = [3,6,1,5]
+bubblesort(array,debug=True)
+print("done with bubblesort")
+array = [3,6,1,5]
+bubblesortOptimized1(array, debug=True)
+print("done with bubblesortOptimized1")
+array = [3,6,1,5]
+bubblesortOptimized2(array, debug=True)
+print("done with bubblesortOptimized2")
