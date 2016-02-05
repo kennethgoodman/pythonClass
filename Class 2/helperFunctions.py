82aab71d322cd966971dac0e405ab8919de565ad
kennethgoodman <kennethsgoodman@yahoo.com>
2016-02-04 23:34:14 -0500

    some examples and changes

--- Class 2/helperFunctions.py
+++ Class 2/helperFunctions.py
@@ -17,9 +17,10 @@ def between(start, end):
 	return list(map(chr, range(ord(start.lower()),ord(end.lower())+1))) + list(map(chr, range(ord(start.upper()), ord(end.upper())+1)))
 
 # examples:
-if 'c' in between('a','d'): #show that it works with lowercase
-	print("c is in between")
-if 'C' in between('a','d'): #works with uppercase
-	print("C is in between")
-if 'f' not in between('a','d'): #show that will sometimes come up false
-	print("f is not in between")
+if __name__ == '__main__':
+	if 'c' in between('a','d'): #show that it works with lowercase
+		print("c is in between")
+	if 'C' in between('a','d'): #works with uppercase
+		print("C is in between")
+	if 'f' not in between('a','d'): #show that will sometimes come up false
+		print("f is not in between")
